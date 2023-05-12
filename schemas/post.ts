9d7cb34fcc7fa1fbe7ba02924f27a090113e3113
Liia Zubova <vardlitmo@gmail.com>
2023-05-12 19:09:15 +0300

    add: sanity blog

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,65 +1,83 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
+export default {
   name: 'post',
   title: 'Post',
   type: 'document',
+  groups: [
+    {
+      name: 'content',
+      title: 'Content',
+    },
+    {
+      name: 'meta',
+      title: 'Meta',
+    },
+  ],
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
+    {
+      name: 'meta_title',
+      title: 'Meta title',
       type: 'string',
-    }),
-    defineField({
+      validation: (Rule: {required: () => any}) => Rule.required(),
+      group: 'meta',
+    },
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      validation: (Rule: {required: () => any}) => Rule.required(),
+      group: 'content',
+    },
+    {
+      name: 'publishedDate',
+      type: 'date',
+      title: 'Published date',
+      validation: (Rule: {required: () => any}) => Rule.required(),
+      group: 'content',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      validation: (Rule: {required: () => any}) => Rule.required(),
+      options: {hotspot: true},
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+        },
+        {
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
+      group: 'content',
+    },
+    {
       name: 'slug',
-      title: 'Slug',
       type: 'slug',
+      title: 'Slyg',
+      validation: (Rule: {required: () => any}) => Rule.required(),
       options: {
         source: 'title',
-        maxLength: 96,
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input: string) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
+      group: 'content',
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'text',
+      validation: (Rule: {required: () => any}) => Rule.required(),
+    },
+    {
+      title: 'Content',
+      name: 'content',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
+      validation: (Rule: {required: () => any}) => Rule.required(),
+      of: [{type: 'block'}, {type: 'image'}],
+    },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
-})
+}
